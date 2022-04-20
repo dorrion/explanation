a0efaed88c10efd72f3811955a721201a272d9e6
Oheun Kim <100553086+dorrion@users.noreply.github.com>
2022-04-21 04:06:47 +0900

    MainActivity

---
+++ new_viewpager/java/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.explanation;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager; // 페이저 객체 생성
+    Button button; // 버튼 객체 생성
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pager = findViewById(R.id.pager);
+
+        //화면 전환 프래그먼트 선언 및 초기 화면 설정
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.ContentLayout, Fragment1.newinstance()).commit();
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Fragment3.class);
+                startActivity(intent);
+            }
+        });
+
+        //페이지 갯수 지정
+        pager.setOffscreenPageLimit(3);
+
+        //어댑터 객체 생성
+        MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager());
+
+        //뷰페이저 프래그먼트 등록
+        Fragment1 fragment1 = new Fragment1();
+        adapter.addItem(fragment1);
+
+        Fragment2 fragment2 = new Fragment2();
+        adapter.addItem(fragment2);
+
+        Fragment3 fragment3 = new Fragment3();
+        adapter.addItem(fragment3);
+
+        pager.setAdapter(adapter);
+
+    }
+
+    class MyPagerAdapter extends FragmentStatePagerAdapter{
+        ArrayList<Fragment> items = new ArrayList<Fragment>();
+
+        public MyPagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        public void addItem(Fragment item){
+            items.add(item);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount(){
+            return items.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "페이지" + position;
+        }
+    }
+
+    public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.ContentLayout, fragment).commit();
+    }
+}
